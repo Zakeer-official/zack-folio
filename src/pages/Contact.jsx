f95d684eace90cf060723e52b2210a448d92d629
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2025-04-26 00:16:04 +0530

    Updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,14 +9,15 @@ export default function Contact() {
 
 			<section className="mapbox" data-mapbox>
 				<figure>
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d418.88270849040924!2d80.13391760670002!3d14.499615077890976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1745606117288!5m2!1sen!2sin" 
-						width="600" 
-						height="450" 
-						style={{ border: 0 }} 
-						allowfullscreen="" 
-						loading="lazy" 
-						referrerpolicy="no-referrer-when-downgrade">
-					</iframe>
+					<iframe
+					  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62273.50597128509!2d79.95370769999999!3d14.442598449999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4caa26b1f67a5b%3A0xaaaeb918cd19fc4f!2sNellore%2C%20Andhra%20Pradesh!5e0!3m2!1sen!2sin!4v1714126217486!5m2!1sen!2sin"
+					  width="600"
+					  height="450"
+					  style={{ border: 0 }}
+					  allowFullScreen=""
+					  loading="lazy"
+					  referrerPolicy="no-referrer-when-downgrade"
+					/>
 				</figure>
 			</section>
 
