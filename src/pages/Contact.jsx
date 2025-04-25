0e9eab1689963e09f1e5de0c9637188d0a7e5fed
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2025-04-26 00:07:16 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,15 +9,14 @@ export default function Contact() {
 
 			<section className="mapbox" data-mapbox>
 				<figure>
-					<iframe
-						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d231366.58479482666!2d74.93395850624634!3d12.905359533802308!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba35a4c37bf488f%3A0x827bbc7a74fcfe64!2sMangaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1721065588811!5m2!1sen!2sin"
-						width="600"
-						height="450"
-						style={{ border: 0 }}
-						allowfullscreen=""
-						loading="lazy"
-						referrerpolicy="no-referrer-when-downgrade"
-					></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d418.88270849040924!2d80.13391760670002!3d14.499615077890976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1745606117288!5m2!1sen!2sin" 
+						width="600" 
+						height="450" 
+						style="border:0;" 
+						allowfullscreen="" 
+						loading="lazy" 
+						referrerpolicy="no-referrer-when-downgrade">
+					</iframe>
 				</figure>
 			</section>
 
